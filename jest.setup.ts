f6a3f5288079cc35d699b5c4aba126610e246e61
FabioSicca <fsicca@fi.uba.ar>
2025-05-11 19:15:35 -0300

    linter

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,7 +2,6 @@ import { Server } from 'http';
 import app from './src/app';
 import { v4 as uuidv4 } from 'uuid';
 import { mockDB } from './src/tests/mocks/mock.db';
-import { getEnrollmentsByCourseId } from './src/database/enrollment_db';
 
 let server: Server;
 
