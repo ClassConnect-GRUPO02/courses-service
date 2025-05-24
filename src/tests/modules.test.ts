dacfb2d89dd0473c08919da9d080ea8909f44c47
FabioSicca <fsicca@fi.uba.ar>
2025-05-24 17:16:46 -0300

    fix tests for modules

--- src/tests/modules.test.ts
+++ src/tests/modules.test.ts
@@ -3,22 +3,26 @@ import app from '../app';
 import { StatusCodes } from 'http-status-codes';
 import { mockCourseRequestData } from './mocks/mock.course';
 import { mockModuleRequestData } from './mocks/mock.module';
+import jwt from 'jsonwebtoken';
+import { userTypes } from '../lib/user_types';
 
 describe('Integration Tests for modules of Courses API', () => {
 
+    const token = jwt.sign(
+    { id: 'u1',
+      userType: userTypes.INSTRUCTOR,
+      }, // payload
+    process.env.SECRET_KEY!, // clave secreta
+    { algorithm: 'HS256' }
+  );
+
   describe('POST /courses/:id/modules', () => {
     it('should add a new module to the course', async () => {
-      // 1. Create new course
-      const courseResponse = await request(app)
-        .post('/courses')
-        .send(mockCourseRequestData);
-
-      const createdCourseId = courseResponse.body.data.id;
-      mockModuleRequestData.courseId = createdCourseId;
-
-      // 2. Add new module to the created course
+    
+      const courseId = 'c1';
       const response = await request(app)
-        .post(`/courses/${createdCourseId}/modules`)
+        .post(`/courses/${courseId}/modules`)
+        .set('Authorization', `Bearer ${token}`)
         .send(mockModuleRequestData);
 
       // 3. Check the response
@@ -29,25 +33,24 @@ describe('Integration Tests for modules of Courses API', () => {
       expect(createdModule.description).toBe(mockModuleRequestData.description);
       expect(createdModule.url).toBe(mockModuleRequestData.url);
       expect(createdModule.order).toBe(mockModuleRequestData.order);
-      expect(createdModule.courseId).toBe(createdCourseId);
+      expect(createdModule.courseId).toBe(courseId);
       expect(createdModule.id).toBeDefined();
     });
 
     it('should return 404 if the course does not exist', async () => {
       const nonExistentCourseId = 'non-existent-course-id';
+      
       const response = await request(app)
         .post(`/courses/${nonExistentCourseId}/modules`)
+        .set('Authorization', `Bearer ${token}`)
         .send(mockModuleRequestData);
 
       expect(response.status).toBe(StatusCodes.NOT_FOUND);
     });
 
     it('should return 400 if the module data is invalid', async () => {
-      const courseResponse = await request(app)
-        .post('/courses')
-        .send(mockCourseRequestData);
 
-      const createdCourseId = courseResponse.body.data.id;
+      const courseId = 'c1';
 
       // Send invalid module data
       const invalidModuleData = {
@@ -55,11 +58,12 @@ describe('Integration Tests for modules of Courses API', () => {
         description: "Missing name",
         url: "https://example.com/intro-typescript",
         order: 1,
-        courseId: createdCourseId,
+        courseId: courseId,
       };
 
       const response = await request(app)
-        .post(`/courses/${createdCourseId}/modules`)
+        .post(`/courses/${courseId}/modules`)
+        .set('Authorization', `Bearer ${token}`)
         .send(invalidModuleData);
 
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
@@ -68,100 +72,66 @@ describe('Integration Tests for modules of Courses API', () => {
 
   describe('GET /courses/:id/modules', () => {
     it('should retrieve all modules for a given course ID', async () => {
-      const courseResponse = await request(app)
-        .post('/courses')
-        .send(mockCourseRequestData);
 
-      const createdCourseId = courseResponse.body.data.id;
-      mockModuleRequestData.courseId = createdCourseId;
-
-      // Add a module to the course
-      await request(app)
-        .post(`/courses/${createdCourseId}/modules`)
-        .send(mockModuleRequestData);
+      const courseId = 'c1';
 
       // Retrieve modules for the course
       const response = await request(app)
-        .get(`/courses/${createdCourseId}/modules`);
+        .get(`/courses/${courseId}/modules`)
+        .set('Authorization', `Bearer ${token}`);
 
       expect(response.status).toBe(StatusCodes.OK);
       expect(Array.isArray(response.body.data)).toBe(true);
-      expect(response.body.data.length).toBe(1);
+      expect(response.body.data.length).toBe(4);
     });
   });
+
   describe('GET /courses/:id/modules/:moduleId', () => {
     it('should retrieve a specific module by ID inside a course', async () => {
-      const courseResponse = await request(app)
-      .post('/courses')
-        .send(mockCourseRequestData);
-        
-      const createdCourseId = courseResponse.body.data.id;
-      mockModuleRequestData.courseId = createdCourseId;
-      
-      // Add a module to the course
-      const moduleResponse = await request(app)
-      .post(`/courses/${createdCourseId}/modules`)
-      .send(mockModuleRequestData);
-      
-      const createdModuleId = moduleResponse.body.data.id;
-      expect(moduleResponse.status).toBe(StatusCodes.CREATED); 
+
+      const courseId = 'c1';
+      const moduleId = 'm1';
       
       // Retrieve the specific module
       const response = await request(app)
-      .get(`/courses/${createdCourseId}/modules/${createdModuleId}`);
+      .get(`/courses/${courseId}/modules/${moduleId}`)
+      .set('Authorization', `Bearer ${token}`)
       
       expect(response.status).toBe(StatusCodes.OK);
-      expect(response.body.data.name).toBe(mockModuleRequestData.name);
       });
       
     it('should return 404 if the module does not exist', async () => {
       const nonExistentModuleId = 'non-existent-module-id';
-      const courseResponse = await request(app)
-      .post('/courses')
-      .send(mockCourseRequestData);
-      
-      const createdCourseId = courseResponse.body.data.id;
-      
+      const courseId = 'c1';
+            
       const response = await request(app)
-      .get(`/courses/${createdCourseId}/modules/${nonExistentModuleId}`);
+      .get(`/courses/${courseId}/modules/${nonExistentModuleId}`)
+      .set('Authorization', `Bearer ${token}`)
       
       expect(response.status).toBe(StatusCodes.NOT_FOUND);
-    }); 
+    });
   });
 
   describe('DELETE /courses/:id/modules/:moduleId', () => {
     it('should delete a module from a course', async () => {
-      const courseResponse = await request(app)
-        .post('/courses')
-        .send(mockCourseRequestData);
-
-      const createdCourseId = courseResponse.body.data.id;
-      mockModuleRequestData.courseId = createdCourseId;
-
-      // Add a module to the course
-      const moduleResponse = await request(app)
-        .post(`/courses/${createdCourseId}/modules`)
-        .send(mockModuleRequestData);
-
-      const createdModuleId = moduleResponse.body.data.id;
+      const courseId = 'c1';
+      const moduleId = 'm2';
 
       // Delete the module
       const response = await request(app)
-        .delete(`/courses/${createdCourseId}/modules/${createdModuleId}`);
+        .delete(`/courses/${courseId}/modules/${moduleId}`)
+        .set('Authorization', `Bearer ${token}`)
 
       expect(response.status).toBe(StatusCodes.NO_CONTENT);
     });
 
     it('should return 404 if the module does not exist', async () => {
       const nonExistentModuleId = 'non-existent-module-id';
-      const courseResponse = await request(app)
-        .post('/courses')
-        .send(mockCourseRequestData);
-
-      const createdCourseId = courseResponse.body.data.id;
+      const courseId = 'c1';
 
       const response = await request(app)
-        .delete(`/courses/${createdCourseId}/modules/${nonExistentModuleId}`);
+        .delete(`/courses/${courseId}/modules/${nonExistentModuleId}`)
+        .set('Authorization', `Bearer ${token}`)
 
       expect(response.status).toBe(StatusCodes.NOT_FOUND);
     });
@@ -171,7 +141,8 @@ describe('Integration Tests for modules of Courses API', () => {
     it('should update the order of modules in a course', async () => {
       const courseResponse = await request(app)
         .post('/courses')
-        .send(mockCourseRequestData);
+        .send(mockCourseRequestData)
+        .set('Authorization', `Bearer ${token}`)
 
       const createdCourseId = courseResponse.body.data.id;
       mockModuleRequestData.courseId = createdCourseId;
@@ -179,10 +150,12 @@ describe('Integration Tests for modules of Courses API', () => {
       // Add multiple modules to the course
       const moduleResponse1 = await request(app)
         .post(`/courses/${createdCourseId}/modules`)
+        .set('Authorization', `Bearer ${token}`)
         .send(mockModuleRequestData);
 
       const moduleResponse2 = await request(app)
         .post(`/courses/${createdCourseId}/modules`)
+        .set('Authorization', `Bearer ${token}`)
         .send({
           ...mockModuleRequestData,
           name: 'Second Module',
@@ -196,10 +169,12 @@ describe('Integration Tests for modules of Courses API', () => {
       const orderedModuleIds = [createdModuleId2, createdModuleId1];
       const response = await request(app)
         .patch(`/courses/${createdCourseId}/modules/order`)
+        .set('Authorization', `Bearer ${token}`)
         .send({ orderedModuleIds });
 
       const getAllModulesResponse = await request(app)
-        .get(`/courses/${createdCourseId}/modules`);
+        .get(`/courses/${createdCourseId}/modules`)
+        .set('Authorization', `Bearer ${token}`);
       const modulesInArray = getAllModulesResponse.body.data;
       const firstModule = modulesInArray[0];
       const secondModule = modulesInArray[1];
@@ -213,6 +188,7 @@ describe('Integration Tests for modules of Courses API', () => {
     it('should return 400 if the orderedModuleIds is not an array', async () => {
       const courseResponse = await request(app)
         .post('/courses')
+        .set('Authorization', `Bearer ${token}`)
         .send(mockCourseRequestData);
 
       const createdCourseId = courseResponse.body.data.id;
@@ -220,6 +196,7 @@ describe('Integration Tests for modules of Courses API', () => {
       // Update the order of modules with invalid data
       const response = await request(app)
         .patch(`/courses/${createdCourseId}/modules/order`)
+        .set('Authorization', `Bearer ${token}`)
         .send({ orderedModuleIds: 'invalid-data' });
 
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
@@ -231,6 +208,7 @@ describe('Integration Tests for modules of Courses API', () => {
       const moduleId = 'm1';
       const response = await request(app)
         .patch(`/courses/c1/modules/${moduleId}`)
+        .set('Authorization', `Bearer ${token}`)
         .send({
           name: 'Updated Module' });
       expect(response.status).toBe(StatusCodes.OK);
